ce82871d63f6c5ea1b9d6f561220e740917ecfa2
yujisatojr <yujisatojr@gmail.com>
2020-12-23 11:08:12 -1000

    initial commit

---
+++ RealEstateListings/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  RealEstateListings
+//
+//  Created by Yuji Sato on 12/23/20.
+//
+
+import SwiftUI
+
+/*struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}*/
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            CardView(image: "todd-kent-178j8tJrNlc-unsplash", category: "[サンプル]ルーブル浦田参番館 102", heading: "¥86,000", author: "Yuji Sato")
+            CardView(image: "note-thanun-k4EfTi2WXLc-unsplash", category: "[サンプル]グランヴァン西落合 307", heading: "¥81,000", author: "Yuji Sato")
+            CardView(image: "kara-eads-L7EwHkq1B2s-unsplash", category: "[サンプル]セレッソコートリバーサイド OSAKA 414", heading: "¥85,600", author: "Yuji Sato")
+            }
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealEstateListings/RealEstateListingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealEstateListingsApp.swift
+//  RealEstateListings
+//
+//  Created by Yuji Sato on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct RealEstateListingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
