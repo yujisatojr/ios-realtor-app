7ea50f942455eca5a7665febfd93827d466f882c
Yuji Sato <yujisatojr@gmail.com>
2022-03-05 23:29:38 -1000

    Add detail view and tab menu

--- RealEstateListings/ContentView.swift
+++ RealEstateListings/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            CardView(image: "todd-kent-178j8tJrNlc-unsplash", category: "[サンプル]ルーブル浦田参番館 102", heading: "¥86,000", author: "Yuji Sato")
-            CardView(image: "note-thanun-k4EfTi2WXLc-unsplash", category: "[サンプル]グランヴァン西落合 307", heading: "¥81,000", author: "Yuji Sato")
-            CardView(image: "kara-eads-L7EwHkq1B2s-unsplash", category: "[サンプル]セレッソコートリバーサイド OSAKA 414", heading: "¥85,600", author: "Yuji Sato")
+        TabView {
+            NavigationView {
+                ScrollView {
+                    NavigationLink(destination: DetailView()) {
+                    CardView(image: "todd-kent-178j8tJrNlc-unsplash", category: "[サンプル]ルーブル浦田参番館 102", heading: "¥86,000", author: "Yuji Sato")
+                    }
+                    NavigationLink(destination: DetailView()) {
+                    CardView(image: "note-thanun-k4EfTi2WXLc-unsplash", category: "[サンプル]グランヴァン西落合 307", heading: "¥81,000", author: "Yuji Sato")
+                    }
+                    NavigationLink(destination: DetailView()) {
+                    CardView(image: "kara-eads-L7EwHkq1B2s-unsplash", category: "[サンプル]セレッソコートリバーサイド OSAKA 414", heading: "¥85,600", author: "Yuji Sato")
+                    }
+                }
+            }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("物件情報")
+            }
+            Text("通知をここに表示")
+                .tabItem {
+                    Image(systemName: "tray.and.arrow.down.fill")
+                    Text("通知")
+            }
+            Text("シミュレーション情報やグラフをここに表示")
+                .tabItem {
+                    Image(systemName: "chart.bar")
+                    Text("シミュレーション")
+            }
+            Text("マイページとメニューを表示")
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                    Text("マイページ")
             }
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
---
+++ RealEstateListings/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  RealEstateListings
+//
+//  Created by Yuji Sato on 3/5/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("物件の詳細情報を表示")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
